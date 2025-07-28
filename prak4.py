3e37a749f5b7b3678ab71621f83f338f36b5d5ba
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-07-28 15:23:22 +0700

    Praktikum 4

---
+++ prak4.py
@@ -0,0 +1,7 @@
+#Write a PYTHON program to print odd numbers up to n!
+
+n = int(input("Masukkan nilai n untuk mencetak bilangan ganjil: "))
+
+print("Bilangan ganjil hingga", n, ":")
+for i in range(1, n+1, 2):
+    print(i, end=" ")
